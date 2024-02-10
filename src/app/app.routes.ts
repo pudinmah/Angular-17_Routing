a086dbefa2e1824872de751be90b76c27a40b3a3
Mahpudin <pudinmah69@gmail.com>
2024-02-10 16:51:54 +0700

    Angular 17 Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {'path': 'home','title':'Home',component:HomeComponent},
+  {'path': 'about','title':'About',component:AboutComponent},
+  {'path': '',redirectTo: '/home', pathMatch: 'full'},
+  {'path': '**',component:PageNotFoundComponent},
+];
